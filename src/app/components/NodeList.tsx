43fc80935fc657dd22d8c7ddd93a903a7ff8ac97
MorEdward <156802593+MorEdward@users.noreply.github.com>
2026-05-26 09:12:36 +0200

    Update NodeList.tsx

--- src/app/components/NodeList.tsx
+++ src/app/components/NodeList.tsx
@@ -13,18 +13,13 @@ function NodeList(props) {
   // Reduce the size of our array of errors by removing
   // nodes with no errors on them.
   let filteredErrorArray = props.errorArray.filter(
-    item => item.errors.length >= 1
+    item => item.errors && item.errors.length >= 1
   );
 
   filteredErrorArray.forEach(item => {
-    // Check each layer/node to see if an error that matches it's layer id
     if (props.ignoredErrorArray.some(x => x.node.id === item.id)) {
-      // When we know a matching error exists loop over all the ignored
-      // errors until we find it.
       props.ignoredErrorArray.forEach(ignoredError => {
         if (ignoredError.node.id === item.id) {
-          // Loop over every error this layer/node until we find the
-          // error that should be ignored, then remove it.
           for (let i = 0; i < item.errors.length; i++) {
             if (item.errors[i].value === ignoredError.value) {
               item.errors.splice(i, 1);
@@ -37,11 +32,9 @@ function NodeList(props) {
   });
 
   const handleNodeClick = id => {
-    // Opens the panel if theres an error.
     let activeId = props.errorArray.find(e => e.id === id);
 
-    if (activeId.errors.length) {
-      // Pass the plugin the ID of the layer we want to fetch.
+    if (activeId && activeId.errors && activeId.errors.length) {
       parent.postMessage(
         { pluginMessage: { type: "fetch-layer-data", id: id } },
         "*"
